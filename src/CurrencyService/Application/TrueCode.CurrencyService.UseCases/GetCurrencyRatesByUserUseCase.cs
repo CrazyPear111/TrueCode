d39e362689279995d13f4dc7a95aaf965ba6569f
Максим Князев <CiberGamer111@gmail.com>
2025-09-20 22:16:58 +0300

    fix GetCurrencyRatesByUserUseCase

--- src/CurrencyService/Application/TrueCode.CurrencyService.UseCases/GetCurrencyRatesByUserUseCase.cs
+++ src/CurrencyService/Application/TrueCode.CurrencyService.UseCases/GetCurrencyRatesByUserUseCase.cs
@@ -5,11 +5,16 @@ namespace TrueCode.CurrencyService.UseCases;
 
 internal class GetCurrencyRatesByUserUseCase : IUseCase<long, Task<Dictionary<string, double>>>
 {
-    private ICurrencyRepository currencyRepository;
+    private ICurrencyRepository _currencyRepository;
+
+    public GetCurrencyRatesByUserUseCase(ICurrencyRepository repository)
+    {
+        _currencyRepository = repository;
+    }
 
     public async Task<Dictionary<string, double>> Invoke(long userId)
     {
-        var favoritesRate = await currencyRepository.GetFavoritesRate(userId);
+        var favoritesRate = await _currencyRepository.GetFavoritesRate(userId);
         return favoritesRate;
     }
 }
